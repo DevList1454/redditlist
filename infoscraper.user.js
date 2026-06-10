a7defb8d9b1c0f390a32515029a0d878f17c6339
DevList1454 <ebb110@outlook.com>
2026-06-10 20:17:34 +0100

    Create infoscraper.user.js

---
+++ infoscraper.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Subreddit Info Scraper
+// @namespace    http://tampermonkey.net/
+// @version      1.11.0
+// @description  Scrapes subreddit status, UI integrated into reddit, Pause/Resume, extracts ban dates.
+// @match        https://old.reddit.com/*
+// @grant        none
+// @updateURL    https://
+// @downloadURL  https://
+// ==/UserScript==
